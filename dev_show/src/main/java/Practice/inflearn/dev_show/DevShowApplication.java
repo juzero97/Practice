953ad2349b8bcf261ddc6318da68880a70ab434c
JUYEONG <juzeroo97@gmail.com>
2024-07-04 16:31:58 +0900

    project setting

---
+++ dev_show/src/main/java/Practice/inflearn/dev_show/DevShowApplication.java
@@ -0,0 +1,13 @@
+package Practice.inflearn.dev_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevShowApplication.class, args);
+	}
+
+}
---
+++ dev_show/src/test/java/Practice/inflearn/dev_show/DevShowApplicationTests.java
@@ -0,0 +1,13 @@
+package Practice.inflearn.dev_show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
